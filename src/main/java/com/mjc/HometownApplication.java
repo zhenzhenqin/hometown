9dd14df2c923983d69a061dfe60fb49c7b2d1023
zhenzhenqin <3378405015@qq.com>
2025-09-13 18:22:23 +0800

    首次提交测试

---
+++ src/main/java/com/mjc/HometownApplication.java
@@ -0,0 +1,13 @@
+package com.mjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HometownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjc/HometownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
