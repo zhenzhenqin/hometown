87035e6e4c5929f62f99ccb756e9e6e1412f4ec6
zhenzhenqin <3378405015@qq.com>
2025-12-16 00:48:00 +0800

    通过redis构建缓存

--- src/main/java/com/mjc/service/Impl/CultureServiceImpl.java
+++ src/main/java/com/mjc/service/Impl/CultureServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mjc.service.Impl;
 
+import cn.hutool.json.JSONUtil;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.mjc.bean.Culture;
@@ -8,16 +9,27 @@ import com.mjc.bean.PageResult;
 import com.mjc.mapper.CultureMapper;
 import com.mjc.service.CultureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.mjc.utils.RedisConstants.CULTURE_QUERY_KEY;
 
 @Service
 public class CultureServiceImpl implements CultureService {
 
     @Autowired
     private CultureMapper cultureMapper;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    //缓存key
+    private final static String key = CULTURE_QUERY_KEY;
 
     /**
      * 获取文化列表
@@ -32,13 +44,16 @@ public class CultureServiceImpl implements CultureService {
         if(cultureQueryParam.getPageSize() == null){
             cultureQueryParam.setPageSize(10);
         }
+
         //配置分页参数，及第几页和每页展示条数
         PageHelper.startPage(cultureQueryParam.getPage(), cultureQueryParam.getPageSize());
 
         //查询
         List<Culture> cultureList = cultureMapper.getCultureLists(cultureQueryParam);
         Page<Culture> page = (Page<Culture>) cultureList; //将查询的文化列表强转为Page对象
-        return new PageResult(page.getTotal(), page.getResult());
+        PageResult pageResult = new PageResult(page.getTotal(), page.getResult());
+
+        return pageResult;
     }
 
     /**
@@ -50,6 +65,7 @@ public class CultureServiceImpl implements CultureService {
         culture.setCreateTime(LocalDateTime.now());
         culture.setUpdateTime(LocalDateTime.now());
         cultureMapper.insertCulture(culture);
+        clearCultureCache();
     }
 
     /**
@@ -59,17 +75,38 @@ public class CultureServiceImpl implements CultureService {
      */
     @Override
     public Culture getCulture(Integer id) {
-        Culture culture = cultureMapper.getById(id);
-        return culture;
+        //设置单个缓存key
+        String keyId = key + id;
+
+        //先根据缓存查询
+        String cultureJson = stringRedisTemplate.opsForValue().get(keyId);
+
+        if (cultureJson != null) {
+            return JSONUtil.toBean(cultureJson, Culture.class);
+        }
+
+        //缓存未命中查询数据库
+        Culture dbCulture = cultureMapper.getById(id);
+
+        //重构缓存
+        stringRedisTemplate.opsForValue().set(keyId, JSONUtil.toJsonStr(dbCulture), Duration.ofMinutes(30));
+
+        return dbCulture;
     }
 
     /*
-    * 修改文化
+     * 修改文化
      */
     @Override
     public void updateCulture(Culture culture) {
         culture.setUpdateTime(LocalDateTime.now());
         cultureMapper.updateCulture(culture);
+
+        //删除单个更新的文化的缓存
+        String keyId = key + culture.getId();
+        stringRedisTemplate.delete(keyId);
+
+        clearCultureCache();
     }
 
     /**
@@ -79,6 +116,15 @@ public class CultureServiceImpl implements CultureService {
     @Override
     public void deleteCulture(List<Integer> ids) {
         cultureMapper.deleteCulture(ids);
+
+        //批量删除缓存
+        List<String> keysToDelete = new ArrayList<>();
+        for (Integer id : ids) {
+            keysToDelete.add(key + id);
+        }
+        stringRedisTemplate.delete(keysToDelete);
+
+        clearCultureCache();
     }
 
     /**
@@ -87,7 +133,26 @@ public class CultureServiceImpl implements CultureService {
      */
     @Override
     public List<Culture> findCulture() {
+        //查询缓存
+        String cultureListJson = stringRedisTemplate.opsForValue().get(key);
+
+        if (cultureListJson != null){
+            return JSONUtil.toList(cultureListJson, Culture.class);
+        }
+
+        //缓存未命中 查询数据库
         List<Culture> cultureList = cultureMapper.findCulture();
+
+        //将数据存入缓存中
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(cultureList), Duration.ofMinutes(30));
+
         return cultureList;
+    }
+
+    /**
+     * 清除文化相关缓存
+     */
+    private void clearCultureCache() {
+        stringRedisTemplate.delete(key);
     }
 }
